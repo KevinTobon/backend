be62defe8bb27d1fad56b51065efdffd9d651adb
kevin.tobon <kevin.tobon@KEVIN-TOBON.ceiba.local>
2019-01-14 13:33:37 -0500

    se hacen pruebas unitarias TimeVehicleTest y se agregan constantes

--- src/main/java/co/com/ceiba/parking/util/Constants.java
+++ src/main/java/co/com/ceiba/parking/util/Constants.java
@@ -21,10 +21,14 @@ public class Constants {
 	public static final String PLATE_VEHICLE_CAR = "FUM78B";
 	public static final int CAPACITY_CARS_LESS = 10;
 	public static final int CAPACITY_MOTORCYCLE_LESS = 9;
-	public static final LocalDateTime ENTRY_DATE_TEST = LocalDateTime.of(2018, 12, 1, 8, 20);
 	public static final LocalDateTime DATE_NOT_PERMITTED_MONDAY_PLATE_A = LocalDateTime.of(2018, 12, 12, 8, 28);
 	public static final LocalDateTime DATE_PERMITTED_MONDAY_PLATE_A = LocalDateTime.of(2018, 12, 17, 8, 28);
 	public static final LocalDateTime DATE_PERMITTED_SUNDAY_PLATE_A = LocalDateTime.of(2018, 12, 16, 8, 28);
+	public static final LocalDateTime ENTRY_DATE_TEST = LocalDateTime.of(2018, 12, 1, 8, 20);
+	public static final LocalDateTime EXIT_DATE_TEST = LocalDateTime.of(2018, 12, 12, 10, 30);
+	public static final LocalDateTime EXIT_DATE_TEST_SECONS = LocalDateTime.of(2018, 12, 13, 11, 01, 01);
+	public static final LocalDateTime EXIT_DATE_TEST_MINUTES = LocalDateTime.of(2018, 12, 12, 8, 19, 10);
+	public static final LocalDateTime EXIT_DATE_TEST_HOURS = LocalDateTime.of(2018, 12, 12, 11, 21, 0);
 
 	
 }
---
+++ src/test/java/co/com/ceiba/parking/TestValidations/TimeVehicleTest.java
@@ -0,0 +1,64 @@
+package co.com.ceiba.parking.TestValidations;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Timestamp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import co.com.ceiba.parking.util.Constants;
+import co.com.ceiba.parking.util.TimeMovement;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TimeVehicleTest {
+
+	@InjectMocks
+	TimeMovement timeMovement;
+	
+	@Test
+	public void calculateTimeOfPayment() {
+	
+		timeMovement.calculateVehicleTime(Timestamp.valueOf(Constants.ENTRY_DATE_TEST), Timestamp.valueOf(Constants.EXIT_DATE_TEST));
+		
+		assertEquals(11, timeMovement.days);
+		assertEquals(3, timeMovement.hours);
+		assertEquals(10, timeMovement.minutes);
+		assertEquals(0, timeMovement.seconds);
+	}
+	
+	@Test
+	public void calculateTimeQuantityWithSecons() {
+		
+		timeMovement.calculateVehicleTime(Timestamp.valueOf(Constants.ENTRY_DATE_TEST), Timestamp.valueOf(Constants.EXIT_DATE_TEST_SECONS));
+		
+		assertEquals(12, timeMovement.days);
+		assertEquals(3, timeMovement.hours);
+		assertEquals(42, timeMovement.minutes);
+		assertEquals(1, timeMovement.seconds);
+	}
+	
+	@Test
+	public void calculateTimeQuantityWithMinutes() {
+		
+		timeMovement.calculateVehicleTime(Timestamp.valueOf(Constants.ENTRY_DATE_TEST), Timestamp.valueOf(Constants.EXIT_DATE_TEST_MINUTES));
+		
+		assertEquals(10, timeMovement.days);
+		assertEquals(24, timeMovement.hours);
+		assertEquals(60, timeMovement.minutes);
+		assertEquals(10, timeMovement.seconds);
+	}
+	
+	@Test
+	public void calculateTimeQuantityWithHours() {
+		
+		timeMovement.calculateVehicleTime(Timestamp.valueOf(Constants.ENTRY_DATE_TEST), Timestamp.valueOf(Constants.EXIT_DATE_TEST_HOURS));
+		
+		assertEquals(11, timeMovement.days);
+		assertEquals(4, timeMovement.hours);
+		assertEquals(1, timeMovement.minutes);
+		assertEquals(0, timeMovement.seconds);
+	}
+}
